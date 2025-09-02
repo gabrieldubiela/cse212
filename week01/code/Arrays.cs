06cef9d1f0f81ad11a4a8a07d1b722978aaef15c
gabrieldubiela <165738698+gabrieldubiela@users.noreply.github.com>
2025-09-02 17:48:11 -0300

    W01 Code: Dynamic Arrays

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -6,6 +6,7 @@ public static class Arrays
     /// integer greater than 0.
     /// </summary>
     /// <returns>array of doubles that are the multiples of the supplied number</returns>
+    
     public static double[] MultiplesOf(double number, int length)
     {
         // TODO Problem 1 Start
@@ -13,7 +14,24 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // SOLUTION
+
+        // Use number of multiples provided as input to create an array with the lenght equal the input.
+        var multiplesArray = new double[length];
+        
+        // Create a loop with the starting number with the multiples, both provided as input
+        for (var i = 1; i <= length; i++)
+        {
+
+        // multiple the number by the multiple of the loop
+        var multipleOf = number * i;
+
+        // Save the result in the array
+            multiplesArray[i-1] = multipleOf;
+        }
+
+        // return the array
+        return multiplesArray; // replace this return statement with your own
     }
 
     /// <summary>
@@ -23,11 +41,32 @@ public static class Arrays
     ///
     /// Because a list is dynamic, this function will modify the existing data list rather than returning a new list.
     /// </summary>
+
     public static void RotateListRight(List<int> data, int amount)
     {
         // TODO Problem 2 Start
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // SOLUTION
+
+        // Get the length of the list
+        var count = data.Count;
+
+        // Extract the last elements of the list and store them in a temporary list.
+        var lastElem = data.GetRange(count - amount, amount);
+
+        // Extract the first elements of the list and store them in a temporary list.
+        var firstElem = data.GetRange(0, count - amount);
+
+        // Clear the list
+        data.Clear();
+
+        // Add the last elements in the beggining
+        data.AddRange(lastElem);
+
+        // Add the first elements in the end.
+        data.AddRange(firstElem);
     }
 }
