bfee59011f9d4d9c2ee29b9fdc3c0f323b4b4dd7
gabrieldubiela <165738698+gabrieldubiela@users.noreply.github.com>
2025-09-18 16:06:04 -0300

    learning activities week 3

--- week03/learn/Translator.cs
+++ week03/learn/Translator.cs
@@ -24,7 +24,7 @@ public class Translator
     /// <returns>fixed array of divisors</returns>
     public void AddWord(string fromWord, string toWord)
     {
-        // ADD YOUR CODE HERE
+        _words[fromWord] = toWord;
     }
 
     /// <summary>
@@ -34,7 +34,18 @@ public class Translator
     /// <returns>The translated word or "???" if no translation is available</returns>
     public string Translate(string fromWord)
     {
-        // ADD YOUR CODE HERE
-        return "";
+		var translateWord = "";
+		
+        if (_words.ContainsKey(fromWord))
+        {
+            translateWord = _words [fromWord];
+        }
+
+        else
+        {
+            translateWord = "???";
+        }
+
+        return translateWord;
     }
 }
