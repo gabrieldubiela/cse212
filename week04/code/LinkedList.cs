d6407187b4bfb3c31a2286733ed7bd4389097af0
Gabriel Dubiela <gabriel.dubiela@hotmail.com>
2025-09-24 00:16:53 -0300

    week4-code

--- week04/code/LinkedList.cs
+++ week04/code/LinkedList.cs
@@ -33,6 +33,21 @@ public class LinkedList : IEnumerable<int>
     public void InsertTail(int value)
     {
         // TODO Problem 1
+        // Create new node
+        Node newNode = new(value);
+        // If the list is empty, then point both head and tail to the new node.
+        if (_head is null)
+        {
+            _head = newNode;
+            _tail = newNode;
+        }
+        // If the list is not empty, then only tail will be affected.
+        else
+        {
+            newNode.Prev = _tail; // Connect new node to the previous tail
+            _tail.Next = newNode; // Connect the previous tail to the new node
+            _tail = newNode; // Update the tail to point to the new node
+        }
     }
 
 
@@ -65,6 +80,22 @@ public class LinkedList : IEnumerable<int>
     public void RemoveTail()
     {
         // TODO Problem 2
+
+        // If the list has only one item in it, then set head and tail 
+        // to null resulting in an empty list.  This condition will also
+        // cover an empty list.  Its okay to set to null again.
+        if (_head == _tail)
+        {
+            _head = null;
+            _tail = null;
+        }
+        // If the list has more than one item in it, then only the tail
+        // will be affected.
+        else if (_tail is not null)
+        {
+            _tail.Prev!.Next = null; // Disconnect the second-to-last node from the last node
+            _tail = _tail.Prev; // Update the tail to point to the second-to-last node
+        }
     }
 
     /// <summary>
@@ -109,6 +140,38 @@ public class LinkedList : IEnumerable<int>
     public void Remove(int value)
     {
         // TODO Problem 3
+
+        // use a loop to find the first node that contains 'value'
+        Node? curr = _head;
+        while (curr is not null)
+        {
+            if (curr.Data == value)
+            {
+                // If the node to be removed is the head
+                if (curr == _head)
+                {
+                    RemoveHead();
+                }
+                // If the node to be removed is the tail
+                else if (curr == _tail)
+                {
+                    RemoveTail();
+                }
+                // If the node to be removed is in the middle
+                else
+                {
+                    // Bypass the current node
+                    curr.Prev!.Next = curr.Next; 
+                    // Bypass the current node
+                    curr.Next!.Prev = curr.Prev; 
+                }
+
+                // Exit after removing the first occurrence
+                return; 
+            }
+
+            curr = curr.Next; // Move to the next node
+        }
     }
 
     /// <summary>
@@ -117,6 +180,20 @@ public class LinkedList : IEnumerable<int>
     public void Replace(int oldValue, int newValue)
     {
         // TODO Problem 4
+
+        // use a loop to find the nodes that contains 'value'
+        Node? curr = _head;
+        while (curr is not null)
+        {
+            if (curr.Data == oldValue)
+            {
+                // Replace the value
+                curr.Data = newValue; 
+            }
+
+            // Move to the next node
+            curr = curr.Next; 
+        }
     }
 
     /// <summary>
@@ -147,7 +224,16 @@ public class LinkedList : IEnumerable<int>
     public IEnumerable Reverse()
     {
         // TODO Problem 5
-        yield return 0; // replace this line with the correct yield return statement(s)
+
+        // Use a loop to get the numbers from tail to head
+        Node? curr = _tail;
+        while (curr is not null)
+        {
+            yield return curr.Data; // Provide (yield) each item to the user
+
+            // Move to the prev node
+            curr = curr.Prev;
+        }
     }
 
     public override string ToString()
--- week04/code/LinkedList_Tests.cs
+++ week04/code/LinkedList_Tests.cs
@@ -1,6 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-// DO NOT MODIFY THIS FILE
+// DO NOT MODIFY THIS FILE  
 
 [TestClass]
 public class InsertTailTests
